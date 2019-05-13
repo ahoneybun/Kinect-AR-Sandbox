40ff0be0afaa576057c9e2ed66afd09f477989bc
findemor <findemor@gmail.com>
2019-05-13 23:06:18 +0200

    proyectos kinect

---
+++ docs/Windows/MultiQRReader/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using ZXing;
+
+namespace MultiQRReader
+{
+    public class Reader
+    {
+        public class ImageSplice
+        {
+
+        }
+
+
+        public List<ImageSplice> SpliceImage()
+        {
+            List<ImageSplice> splices = new List<ImageSplice>();
+
+            
+
+            return splices;
+        }
+
+        public void TryRead(List<ImageSplice> splices)
+        {
+            // create a barcode reader instance
+            IBarcodeReader reader = new BarcodeReader();
+            // load a bitmap
+            var barcodeBitmap = (Bitmap)Bitmap.LoadFrom("C:\\sample-barcode-image.png");
+            // detect and decode the barcode inside the bitmap
+            var result = reader.Decode(
+            // do something with the result
+            if (result != null)
+            {
+                txtDecoderType.Text = result.BarcodeFormat.ToString();
+                txtDecoderContent.Text = result.Text;
+            }
+        }
+    }
+}
---
+++ docs/Windows/Resources/README.txt
@@ -0,0 +1,28 @@
+# Instalar Kinect XBox 360 (v1) en Windows 10
+
+https://social.msdn.microsoft.com/Forums/en-US/73ef4c8c-fc4b-4321-9d6b-6c147bca6ba5/360-kinect-worked-with-windows-7-but-not-with-windows-10?forum=kinectv2sdk
+
+
+Installing KinectSDK-v1.8-Setup.exe & KinectDeveloperToolkit-v1.8.0-Setup.exe with Kompatibility Settings Windows 7
+Then in Visual Studio 2017 I recompiled all of the samples provided by KinectDeveloperToolkit (C#, C++)
+
+Enlaces: 
+* https://download.microsoft.com/download/E/1/D/E1DEC243-0389-4A23-87BF-F47DE869FC1A/KinectSDK-v1.8-Setup.exe
+* https://download.microsoft.com/download/D/0/6/D061A21C-3AF3-4571-8560-4010E96F0BC8/KinectDeveloperToolkit-v1.8.0-Setup.exe
+
+
+# Instrucciones de desarrollo v1.8
+
+https://digitalerr0r.wordpress.com/2011/06/20/kinect-fundamentals-1-installation-setup/
+
+
+
+
+# TCP
+
+## Unity
+https://gist.github.com/danielbierwirth/0636650b005834204cb19ef5ae6ccedb
+
+## .NET
+
+http://www.codingvision.net/networking/c-simple-tcp-server
